3732f601c501c17f544049fe34b215440d314d0f
dwiantorosk <dwi@vcgamers.com>
2026-04-11 20:21:45 +0700

    UI: Strengthen Islamic background ornaments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,9 +105,13 @@ export default function RootLayout({
         />
       </head>
       <body className="min-h-full flex flex-col pb-24">
-        <AzanReminderController />
-        {children}
-        <BottomNav />
+        <div className="islamic-bg-layer" aria-hidden="true" />
+        <div className="islamic-bg-arch" aria-hidden="true" />
+        <div className="app-shell min-h-full flex flex-col pb-24">
+          <AzanReminderController />
+          {children}
+          <BottomNav />
+        </div>
       </body>
     </html>
   );
