b8f41537c752a25ff0ffe401cd8f5f6ce2ec7b28
dwiantorosk <dwi@vcgamers.com>
2026-04-11 22:12:16 +0700

    chore: disallow all pages from bot crawling in robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,22 +2,10 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        disallow: [
-          '/api/',
-          '/search',
-          '/notes',
-          '/tracker',
-          '/qibla',
-        ],
-      },
-      {
-        userAgent: '*',
-        allow: '/',
-      },
-    ],
+    rules: {
+      userAgent: '*',
+      disallow: '/',
+    },
     sitemap: 'https://muslim-traveler.vercel.app/sitemap.xml',
   };
 }
