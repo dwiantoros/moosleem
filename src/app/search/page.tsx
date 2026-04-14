ac7c4eca54d9038750ffee2d2764f63e5d40c02e
dwiantorosk <dwi@vcgamers.com>
2026-04-14 21:33:00 +0700

    fix: add missing Link import in search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useEffect, useMemo, useState } from 'react';
+import Link from 'next/link';
 import axios from 'axios';
 import QuranStyleHeader from '@/components/QuranStyleHeader';
 
